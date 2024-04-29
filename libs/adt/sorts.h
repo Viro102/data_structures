82114fa1a3be92543353c70956994dac9354495e
Adam Virostek <adam.virostek@gmail.com>
2024-04-29 13:40:35 +0200

    WIP sorts implementation

--- libs/adt/sorts.h
+++ libs/adt/sorts.h
@@ -6,6 +6,7 @@
 #include <algorithm>
 #include <cmath>
 #include <functional>
+#include <numeric>
 #include <type_traits>
 
 namespace ds::adt {
@@ -83,7 +84,7 @@ template <typename Key, typename T> class RadixSort : public Sort<T> {
   public:
     RadixSort();
 
-    RadixSort(std::function<Key(const T &)> getKey);
+    explicit RadixSort(std::function<Key(const T &)> getKey);
 
     void sort(amt::ImplicitSequence<T> &is,
               std::function<bool(const T &, const T &)> compare) override;
@@ -104,7 +105,6 @@ template <typename T> class MergeSort : public Sort<T> {
 
     void merge(std::function<bool(const T &, const T &)> compare, size_t n);
 
-  private:
     ImplicitQueue<T> *queue1_{nullptr};
     ImplicitQueue<T> *queue2_{nullptr};
     ImplicitQueue<T> *mergeQueue_{nullptr};
@@ -115,25 +115,48 @@ template <typename T> class MergeSort : public Sort<T> {
 template <typename T>
 void SelectSort<T>::sort(amt::ImplicitSequence<T> &is,
                          std::function<bool(const T &, const T &)> compare) {
-    // TODO 12
-    // po implementacii vymazte vyhodenie vynimky!
-    throw std::runtime_error("Not implemented yet");
+    for (size_t i = 0; i < is.size(); ++i) {
+        size_t minIndex = i;
+        for (size_t j = i + 1; j < is.size(); ++j) {
+            if (compare(is.access(j)->data_, is.access(minIndex)->data_)) {
+                minIndex = j;
+            }
+        }
+        if (minIndex != i) {
+            std::swap(is.access(i)->data_, is.access(minIndex)->data_);
+        }
+    }
 }
 
 template <typename T>
 void InsertSort<T>::sort(amt::ImplicitSequence<T> &is,
                          std::function<bool(const T &, const T &)> compare) {
-    // TODO 12
-    // po implementacii vymazte vyhodenie vynimky!
-    throw std::runtime_error("Not implemented yet");
+    for (size_t i = 1; i < is.size(); ++i) {
+        T key = is.access(i)->data_;
+        size_t j = i - 1;
+
+        while (j != static_cast<size_t>(-1) && compare(key, is.access(j)->data_)) {
+            is.access(j + 1)->data_ = is.access(j)->data_;
+            j--;
+        }
+        is.access(j + 1)->data_ = key;
+    }
 }
 
 template <typename T>
 void BubbleSort<T>::sort(amt::ImplicitSequence<T> &is,
                          std::function<bool(const T &, const T &)> compare) {
-    // TODO 12
-    // po implementacii vymazte vyhodenie vynimky!
-    throw std::runtime_error("Not implemented yet");
+    bool swapped;
+    do {
+        swapped = false;
+        for (size_t i = 0; i < is.size() - 1; ++i) {
+            if (compare(is.access(i + 1)->data_, is.access(i)->data_)) {
+                using std::swap;
+                swap(is.access(i + 1)->data_, is.access(i)->data_);
+                swapped = true;
+            }
+        }
+    } while (swapped);
 }
 
 template <typename T>
@@ -148,17 +171,59 @@ template <typename T>
 void QuickSort<T>::quick(amt::ImplicitSequence<T> &is,
                          std::function<bool(const T &, const T &)> compare, size_t min,
                          size_t max) {
-    // TODO 12
-    // po implementacii vymazte vyhodenie vynimky!
-    throw std::runtime_error("Not implemented yet");
+    T pivot = is.access(std::midpoint(min, max))->data_;
+    size_t left = min;
+    size_t right = max;
+
+    while (left <= right) {
+        while (compare(is.access(left)->data_, pivot)) {
+            ++left;
+        }
+        while (right > 0 && compare(pivot, is.access(right)->data_)) {
+            --right;
+        }
+
+        if (left <= right) {
+            std::swap(is.access(left)->data_, is.access(right)->data_);
+            ++left;
+            if (right > 0) {
+                --right;
+            }
+        }
+    }
+    if (min < right) {
+        quick(is, compare, min, right);
+    }
+    if (left < max) {
+        quick(is, compare, left, max);
+    }
 }
 
 template <typename T>
 void HeapSort<T>::sort(amt::ImplicitSequence<T> &is,
                        std::function<bool(const T &, const T &)> compare) {
-    // TODO 12
-    // po implementacii vymazte vyhodenie vynimky!
-    throw std::runtime_error("Not implemented yet");
+    // FIXME
+    // 1. Get the size of the sequence
+    size_t n = is.size();
+
+    // 2. Create a temporary vector to hold the data from the sequence
+    std::vector<T> arr(n);
+    std::copy(is.begin(), is.end(), arr.begin());
+
+    // 3. Build the heap using std::make_heap
+    std::make_heap(arr.begin(), arr.end(), compare);
+
+    // 4. Extract elements one by one using std::heap_pop
+    for (int i = n - 1; i > 0; i--) {
+        // Move current root (largest element) to end
+        std::swap(arr[0], arr[i]);
+
+        // Reduce the heap size and maintain it with std::heap_pop
+        std::pop_heap(arr.begin(), arr.begin() + i, compare);
+    }
+
+    // 5. Copy the sorted data back to the implicit sequence
+    std::copy(arr.begin(), arr.end(), is.begin());
 }
 
 template <typename T>
@@ -170,9 +235,17 @@ void ShellSort<T>::sort(amt::ImplicitSequence<T> &is,
 template <typename T>
 void ShellSort<T>::shell(amt::ImplicitSequence<T> &is,
                          std::function<bool(const T &, const T &)> compare, size_t k) {
-    // TODO 12
-    // po implementacii vymazte vyhodenie vynimky!
-    throw std::runtime_error("Not implemented yet");
+    for (size_t gap = k; gap > 0; gap /= 2) {
+        for (size_t i = gap; i < is.size(); i += 1) {
+            T temp = is.access(i)->data_;
+
+            size_t j;
+            for (j = i; j >= gap && compare(temp, is.access(j - gap)->data_); j -= gap)
+                is.access(j)->data_ = is.access(j - gap)->data_;
+
+            is.access(j)->data_ = temp;
+        }
+    }
 }
 
 template <typename Key, typename T>
